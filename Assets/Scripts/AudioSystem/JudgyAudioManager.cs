34fac04e35cd8ff682bd51162ac47ac70e16cc43
Djordje Cvijic <djordje.cvijic.etf@gmail.com>
2026-01-31 18:08:20 +0100

    add utils & audio manager

---
+++ Assets/Scripts/AudioSystem/JudgyAudioManager.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections;
+using Audio;
+using UnityEngine;
+
+public class JudgyAudioManager : AudioManager
+{
+    public new static JudgyAudioManager Instance => (JudgyAudioManager)AudioManager.Instance;
+
+    [Header("MainMenuMusic")]
+    [SerializeField] private AudioClipSettings mainMenuStart;
+    [SerializeField] private AudioClipSettings mainMenuLoop;
+
+    [Header("LevelMusic")]
+    [SerializeField] private float musicFadeDuration = 1;
+    [SerializeField] private AudioClipSettings levelBase;
+    [SerializeField] private AudioClipSettings[] levelStates;
+
+    [Header("WinCheers")]
+    [SerializeField] private AudioClipSettings[] winCheers;
+
+    [Header("LoseBoos")]
+    [SerializeField] private AudioClipSettings[] loseBoos;
+
+    [Header("CharacterSelection")]
+    [SerializeField] private AudioClipSettings selectComedian;
+    [SerializeField] private AudioClipSettings selectWhisperer;
+
+    [Header("Misc")]
+    [SerializeField] private AudioClipSettings pageFlip;
+    [SerializeField] private AudioClipSettings buttonClick;
+
+    private int _currentState;
+    private bool _mainMenuMusicPlaying;
+    private Coroutine _startMainMenuLoopCoroutine;
+
+    public void StartMainMenuMusic()
+    {
+        StopAudio(levelBase);
+        foreach (var state in levelStates)
+        {
+            StopAudio(state);
+        }
+
+        if (!_mainMenuMusicPlaying)
+        {
+            _mainMenuMusicPlaying = true;
+            PlayAudio(mainMenuStart);
+            _startMainMenuLoopCoroutine = StartCoroutine(StartMainMenuLoop());
+        }
+    }
+
+    private IEnumerator StartMainMenuLoop()
+    {
+        yield return new WaitForSeconds(mainMenuStart.Variants[0].length);
+
+        StopAudio(mainMenuStart);
+        PlayAudio(mainMenuLoop);
+    }
+
+    public void StartLevelMusic()
+    {
+        StopAudio(mainMenuStart);
+        StopAudio(mainMenuLoop);
+        _mainMenuMusicPlaying = false;
+
+        if (_startMainMenuLoopCoroutine != null)
+        {
+            StopCoroutine(_startMainMenuLoopCoroutine);
+            _startMainMenuLoopCoroutine = null;
+        }
+
+        PlayAudio(levelBase);
+        foreach (var state in levelStates)
+        {
+            PlayAudio(state);
+        }
+
+        _currentState = 1;
+    }
+
+    public void FadeToState(int state)
+    {
+        if (state == _currentState) return;
+
+        FadeAudio(levelStates[_currentState - 1], 0, musicFadeDuration);
+        FadeAudio(levelStates[state - 1], 1, musicFadeDuration);
+        _currentState = state;
+    }
+
+    public void WinCheer(float crowdSize01)
+    {
+        var index = (int)(crowdSize01 * winCheers.Length);
+        index = Math.Clamp(index, 0, winCheers.Length - 1);
+
+        var settings = winCheers[index];
+        PlayAudio(settings);
+    }
+
+    public void LoseBoo(float crowdSize01)
+    {
+        var index = (int)(crowdSize01 * loseBoos.Length);
+        index = Math.Clamp(index, 0, loseBoos.Length - 1);
+
+        var settings = loseBoos[index];
+        PlayAudio(settings);
+    }
+
+    public void SelectComedian() => PlayAudio(selectComedian);
+
+    public void SelectWhisperer() => PlayAudio(selectWhisperer);
+
+    public void FlipPage() => PlayAudio(pageFlip);
+
+    public void ClickButton() => PlayAudio(buttonClick);
+}
---
+++ Assets/Scripts/Util/Extensions.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = System.Random;
+
+public static class Extensions
+{
+    public static void ClearChildren(this Transform transform)
+    {
+        foreach (Transform child in transform)
+        {
+            UnityEngine.Object.Destroy(child.gameObject);
+        }
+    }
+
+    public static void SetListener(this UnityEvent unityEvent, UnityAction action)
+    {
+        unityEvent.RemoveAllListeners();
+        unityEvent.AddListener(action);
+    }
+
+    public static void Shuffle<T>(this IList<T> list, string seed)
+    {
+        int? seedInt = null;
+        if (seed != null)
+        {
+            var md5Hasher = MD5.Create();
+            var hashed = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(seed));
+            seedInt = BitConverter.ToInt32(hashed, 0);
+        }
+        
+        list.Shuffle(seedInt);
+    }
+
+    public static void Shuffle<T>(this IList<T> list, int? seedInt = null)
+    {
+        var rng = seedInt.HasValue ? new Random(seedInt.Value) : new Random();
+        var n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            var k = rng.Next(n + 1);
+            (list[k], list[n]) = (list[n], list[k]);
+        }
+    }
+}
