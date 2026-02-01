cc74ee683cf2d5cf3d09e65d61d7d2753ad88020
Petar <petarkovrlija1995@gmail.com>
2026-02-01 14:44:00 +0100

    tweaks to alertpopup and credits scene

--- Assets/Scripts/Util/AlertPopup.cs
+++ Assets/Scripts/Util/AlertPopup.cs
@@ -10,11 +10,24 @@ public class AlertPopup : MonoBehaviour
     [SerializeField] private Button confirmButton;
     [SerializeField] private Button cancelButton;
     [SerializeField] private Animator animator;
+    [SerializeField] private GameObject scoreBackground;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private Sprite letsGo;
+    [SerializeField] private Sprite playAgain;
 
     private Action<bool> _callback;
 
-    public void Open(string text, Action<bool> callback)
+    public void Open(string text, Action<bool> callback, string score = null)
     { 
+        if(score == null){
+            scoreBackground.SetActive(false);
+            confirmButton.GetComponent<Image>().sprite = letsGo;
+        }
+        else{
+            scoreBackground.SetActive(true);
+            confirmButton.GetComponent<Image>().sprite = playAgain;
+            scoreText.text = score;
+        }
         gameObject.SetActive(true);
         textField.text = text;
         _callback = callback;
